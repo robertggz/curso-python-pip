36931f718cf0a9eb403288da7dfda8a2155d11a9
Roberto Guevara <robertggz17@gmail.com>
2024-03-22 23:40:39 -0500

    add docs

---
+++ app/main.py
@@ -0,0 +1,30 @@
+import utils
+import read_csv 
+import charts
+
+def run():
+  data = read_csv.read_csv('data.csv')
+
+  data = list(filter(lambda item: item['Continent'] == 'South America', data))
+
+  countries = list(map(lambda x: x['Country/Territory'], data))
+  porcentages = list(map(lambda x: x['World Population Percentage'], data))
+
+  charts.generate_pie_chart(countries, porcentages)
+
+  #hasta aqui
+  
+  country = input('Type country => ')
+  print(country)
+  
+  result = utils.population_by_country(data, country)
+
+  if len (result) > 0:
+    country = result[0]
+    print(country)
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(country['country'], labels, values)
+    
+
+if __name__== '__main__':
+  run ()
